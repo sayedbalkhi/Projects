dc83f80ceeb6864fc94d1d10cb572fc386759ae2
sayedbalkhi <31773885+sayedbalkhi@users.noreply.github.com>
2018-10-04 16:00:46 -0500

    Update AverageDemo.java

--- AverageDemo.java
+++ AverageDemo.java
@@ -3,6 +3,7 @@ package javabundel.calculateaverage.cognnt;
 public class AverageDemo {
 
 	public static void main(String[] args) {
+		// finding the average 
 		int[] numbers = new int[]{20, 30, 25, 35, -16, 60, -100};
 		
 		int sum=0;
